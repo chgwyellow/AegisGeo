5a520af2a9167d22d0d445a5208ce41d3b139529
chgwyellow <dinnis1107@gmail.com>
2026-07-06 17:01:57 +0800

    feat: implement JMA API client for data ingestion

--- internal/ingestion/jma.go
+++ internal/ingestion/jma.go
@@ -1,1 +1,21 @@
 package ingestion
+
+import (
+	"AegisGeo/internal/models"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+	"time"
+)
+
+type jmaClient struct {
+	apiURL string
+}
+
+func NewJmaClient(apiURL string) *jmaClient {
+	return &jmaClient{
+		apiURL: apiURL,
+	}
+}
